487ce47449196f2d3231be1621c667d93e4bdad8
l-marcel <lmgh1312@gmail.com>
2024-10-29 11:27:51 -0300

    Testando teclas

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 import src.router.Router;
 import src.router.pages.MainPage;
+import src.utils.Terminal;
 
 public class Main {
     public static void main(String[] args) {
-        Router router = new Router();
-        router.start(new MainPage());
+        Terminal terminal = new Terminal();
+        System.out.println(terminal.key());
+        // Router router = new Router();
+        // router.start(new MainPage());
     }
 };
 
